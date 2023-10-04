7e0415265c963f78cb61261c06f05fb272c95d50
Lange Rabiya <rabiyusha@mail.ru>
2023-10-04 17:02:39 +0300

    Initial Commit

---
+++ swift_16/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_16
+//
+//  Created by Рабия Ланге on 04.10.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print ("Mi first commit")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
